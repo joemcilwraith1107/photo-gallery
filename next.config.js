b5c9373692ab174dd67d52909f916983c96564f3
joe.mcilwraith <joe.mcilwraith@remarkable.net>
2022-02-09 12:48:47 +0000

    changed device sizes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    deviceSizes: [320, 420, 768, 1024, 1200],
+    deviceSizes: [320, 420, 768, 1024, 1200, 1920, 2160],
     domains: ['ik.imagekit.io'],
     loader: "default"
   },
