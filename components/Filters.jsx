67bf6c5911a88d10ff0cfb2ed164a95fc2962484
joe.mcilwraith <joe.mcilwraith@remarkable.net>
2022-02-09 18:34:50 +0000

    changed to harcoded url

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -5,7 +5,8 @@ export default function Filters({ tags, setPhotos }) {
     const menu = tags.map((filter) => (
         <button key={filter} className="flex-auto flex-nowrap p-1 text-xs sm:text-sm lg:text-base text-center font-normal text-gray-600 hover:text-gray-900 active:text-black" value={filter} onClick={
             async () => {
-                const results = await fetch(`${process.env.IK_API}?tags=${filter}&sort=DESC_NAME`, {
+                console.log(process.env.IK_API);
+                const results = await fetch(`https://api.imagekit.io/v1/files?tags=${filter}&sort=DESC_NAME`, {
                     headers: {
                         Authorization: `${process.env.PRIVATE_HEADER}`
                     }
