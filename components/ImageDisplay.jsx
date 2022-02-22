89323a9bbc38a522d56d25ca4f254f15baa48d95
joe.mcilwraith <joe.mcilwraith@remarkable.net>
2022-02-22 16:34:53 +0000

    conditional formatting for modal

--- components/ImageDisplay.jsx
+++ components/ImageDisplay.jsx
@@ -2,16 +2,17 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function ImageDisplay({ modal, photo, caption }) {
-
+    const bgClass = "flex flex-col h-full w-full";
+    const fontClass = "font-sans text-2xl";
     return (
-        <div className="flex flex-col h-full w-full bg-slate-800 bg-opacity-70">
+        <div className={modal == true ? `${bgClass} bg-black bg-opacity-80` : `${bgClass} bg-white`}>
             <div className="flex flex-row flex-none basis-1/12">
                 <div className="basis-1/12"></div>
                 <div className="basis-11/12 self-center">
                     {modal ? (<div></div>) : (
                         <Link href="/">
                             <a>
-                                <p className="font-sans text-3xl text-white">
+                                <p className={modal == true ? `${fontClass} text-white` : `${fontClass} text-black`}>
                                     &#x21E6; Back to Gallery
                                 </p>
                             </a>
@@ -38,7 +39,7 @@ export default function ImageDisplay({ modal, photo, caption }) {
             <div className="flex flex-row justify-center basis-1/12">
                 <div className="flex flex-none basis-1/12 justify-center"></div>
                 <div className="flex basis-11/12 justify-center self-center">
-                    <p className="font-sans text-lg text-white">
+                    <p className={modal == true ? `${fontClass} text-white` : `${fontClass} text-black`}>
                         {caption}
                     </p>
                 </div>
