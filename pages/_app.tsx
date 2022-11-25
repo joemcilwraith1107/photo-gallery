4234b4542600b2ae7c1532b33d8ad9925671b78b
joe.mcilwraith <joe.mcilwraith@remarkable.net>
2022-11-25 18:02:36 +0000

    added AppProps type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import '../styles/globals.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import { AppProps } from 'next/app';
 config.autoAddCss = false
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
