2a766c001c930e3a1ca6eae03f5822fafd8c8f7d
joe.mcilwraith <joe.mcilwraith@remarkable.net>
2022-11-25 17:27:14 +0000

    added title to Head

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,18 @@
-import Footer from './Footer';
-import Head from 'next/head';
-import ScrollToTop from './ScrollToTop';
+import Footer from './Footer'
+import Head from 'next/head'
+import ScrollToTop from './ScrollToTop'
 
-export default function Layout({ children }) {
-    return (
-        <div className="flex flex-col h-screen justify-between">
-            <Head></Head>
+export default function Layout({ children }: React.PropsWithChildren) {
+  return (
+    <div className="flex h-screen flex-col justify-between">
+      <Head>
+        <title>Photo Gallery</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
 
-            <main className="basis-11/12 shrink">{children}</main>
-            <ScrollToTop />
-            <Footer />
-        </div>
-    )
+      <main className="shrink basis-11/12">{children}</main>
+      <ScrollToTop />
+      <Footer />
+    </div>
+  )
 }
