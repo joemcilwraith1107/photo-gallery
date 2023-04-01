3bfd4aa3d6cb3f7008b1ba4790886ffe63394a7c
joe.mcilwraith <joe.mcilwraith@remarkable.net>
2023-04-01 18:18:13 +0100

    started again

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,16 @@ import ScrollToTop from './components/ScrollToTop'
 
 config.autoAddCss = false
 
+export const metadata = {
+  title: "Photo Gallery",
+  description: "A photo gallery using nextjs",
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
+}
+
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
