6d3380fad05090fc5fe1a0269c56b10c3350163e
joe.mcilwraith <joe.mcilwraith@remarkable.net>
2023-07-15 17:55:52 +0100

    added icon for modal

--- components/ImageDisplay.tsx
+++ components/ImageDisplay.tsx
@@ -1,7 +1,5 @@
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import Image from 'next/image'
-import Link from 'next/link'
 
 export default function ImageDisplay({ modal, photo }: ImageDisplay) {
   const extraClasses = modal
@@ -10,7 +8,27 @@ export default function ImageDisplay({ modal, photo }: ImageDisplay) {
   return (
     <>
       {modal ? (
-        <div className="flex max-h-screen-80 max-w-screen-80 flex-col bg-white p-4">
+        <div className="relative flex max-h-screen-80 max-w-screen-80 flex-col bg-white p-4">
+          <div className="absolute inset-0 mx-auto flex flex-col">
+            <div className="right-0 top-0 flex items-center p-4">
+              <div className="rounded-full bg-black/60 p-4 text-white/75 hover:bg-black hover:text-white">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
           <Image
             priority={true}
             height={photo.height}
