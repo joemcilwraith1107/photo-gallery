ca9b0b08894bf132711fa7f9e0264389553f4bcf
joe.mcilwraith <joe.mcilwraith@remarkable.net>
2023-11-04 18:30:32 +0000

    setup changes

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,13 @@ const config = {
   reactStrictMode: false,
   images: {
     deviceSizes: [320, 420, 768, 1024, 1200, 1920, 2160],
-    domains: ['ik.imagekit.io'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        pathname: '/ziadpvldoaq/Portfolio/**'
+      },
+    ],
     loader: "default"
   },
 }
