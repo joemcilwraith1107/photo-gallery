19c3b38d8b464d7a7319de3d1c080454b0e2c9e0
joe.mcilwraith <joe.mcilwraith@remarkable.net>
2023-11-24 10:46:33 +0000

    animations

--- components/ImageDisplay.tsx
+++ components/ImageDisplay.tsx
@@ -8,14 +8,22 @@ import BackButton from '@components/BackButton'
 
 export default function ImageDisplay({ modal, photo }: ImageDisplay) {
   const [loaded, setLoaded] = useState(false)
-
+  const containerAnimate = {
+    full: {
+      height: loaded ? '100%' : '150px',
+      width: loaded ? '100%' : '150px',
+      opacity: loaded ? 1 : 0 ,
+      transition: {
+        stagger: 0.5,
+        ease: 'easeInOut',
+        duration: 0.75,
+      },
+    },
+  }
   return (
     <>
-      <motion.div
+      <div
         className={`relative flex flex-col max-h-90 max-w-90 min-w-[150px] min-h-[150px] bg-white p-4`}
-        animate={{ opacity: 1 }}
-        initial={{ opacity: 0 }}
-        transition={{ ease: 'easeInOut', duration: 0.75 }}
       >
         <div
           className={
@@ -30,20 +38,25 @@ export default function ImageDisplay({ modal, photo }: ImageDisplay) {
             </Link>
           )}
         </div>
-
-        <Image
-          priority={true}
-          className="h-full w-full"
-          height={photo.height}
-          width={photo.width}
-          sizes="100vw"
-          src={photo.url}
-          alt={photo.customMetadata.Caption}
-          onLoad={() => {
-            setLoaded(true)
-          }}
-        />
-      </motion.div>
+        <motion.div
+          className={'h-full w-full'}
+          animate="full"
+          variants={containerAnimate}
+        >
+          <Image
+            priority={true}
+            className="h-full w-full"
+            height={photo.height}
+            width={photo.width}
+            sizes="100vw"
+            src={photo.url}
+            alt={photo.customMetadata.Caption}
+            onLoad={() => {
+              setLoaded(true)
+            }}
+          />
+        </motion.div>
+      </div>
       <motion.div
         className={loaded === true ? `relative max-h-[10%] w-auto` : `hidden`}
         animate={{ opacity: 1 }}
