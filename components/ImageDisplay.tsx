c02fff912f799fb44d8a35e37706feacd789c42e
joe.mcilwraith <joe.mcilwraith@remarkable.net>
2023-11-27 17:11:02 +0000

    fix for chrome

--- components/ImageDisplay.tsx
+++ components/ImageDisplay.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from 'next/image'
-import { motion,  } from 'framer-motion'
+import { motion } from 'framer-motion'
 import { useState } from 'react'
 import Link from 'next/link'
 import BackButton from '@components/BackButton'
@@ -19,8 +19,8 @@ export default function ImageDisplay({ modal, photo }: ImageDisplay) {
       className={`flex flex-col items-center justify-center`}
       initial={{ height: '200px', width: '200px' }}
       animate={{
-        height: loading ? '200px' : '100%',
-        width: loading ? '200px' : '100%',
+        height: loading ? '200px' : '100vh',
+        width: loading ? '200px' : '100vw',
       }}
       transition={{
         height: { delay: 0, duration: 1 },
@@ -30,10 +30,10 @@ export default function ImageDisplay({ modal, photo }: ImageDisplay) {
       <div
         className={
           pulsing
-          ? `relative flex flex-col max-h-90 max-w-90 min-w-[150px] min-h-[150px] p-4 animate-pulse bg-gray-300`
-          : `relative flex flex-col max-h-90 max-w-90 min-w-[150px] min-h-[150px] h-screen w-auto bg-white p-4`
+            ? `relative max-h-90 max-w-90 min-w-[150px] min-h-[150px] p-4 animate-pulse bg-gray-300`
+            : `relative max-h-90 max-w-90 min-w-[150px] min-h-[150px] bg-white p-4`
         }
-        >
+      >
         <motion.div
           className={`absolute inset-0 mx-auto flex flex-col z-50`}
           initial={{ opacity: 0 }}
@@ -41,7 +41,7 @@ export default function ImageDisplay({ modal, photo }: ImageDisplay) {
             opacity: loading ? 0 : 1,
           }}
           transition={{ opacity: { delay: 1, duration: 0.5 } }}
-          >
+        >
           {modal ? (
             <BackButton />
           ) : (
@@ -73,10 +73,10 @@ export default function ImageDisplay({ modal, photo }: ImageDisplay) {
       <motion.div
         className={`relative max-h-[10%] w-auto`}
         initial={{ opacity: 0 }}
-          animate={{
-            opacity: loading ? 0 : 1,
-          }}
-          transition={{ opacity: { delay: 1, duration: 0.5 } }}
+        animate={{
+          opacity: loading ? 0 : 1,
+        }}
+        transition={{ opacity: { delay: 1, duration: 0.5 } }}
       >
         <p
           className={
