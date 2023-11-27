90057963eab86244ecc69f58fc0ddbaaeb38f5c5
joe.mcilwraith <joe.mcilwraith@remarkable.net>
2023-11-27 12:45:20 +0000

    changed sizing on scroll to top

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -24,7 +24,7 @@ export default function ScrollToTop() {
   }, [checkScrollTop])
 
   const iconClass =
-    'self-center fixed bottom-1/10 right-20 w-20 h-20 align-middle justify-center opacity-50 z-50 bg-gray-600 rounded-2xl hover:opacity-90 transition-opacity duration-500'
+    'self-center fixed align-middle justify-center opacity-50 z-50 bg-gray-600 rounded-2xl hover:opacity-90 transition-opacity duration-500 bottom-28 right-8 w-14 h-14 md:bottom-28 md:right-10 md:w-20 md:h-20 2xl:right-20'
 
   return (
     <div
@@ -36,8 +36,7 @@ export default function ScrollToTop() {
     >
       <FontAwesomeIcon
         icon={faArrowUp}
-        className="self-center"
-        size="3x"
+        className="self-center text-xl md:text-4xl "
         color="white"
         onClick={scrollTop}
       />
