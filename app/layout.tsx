76dfbc661c06ac0c17c9c0d43743bbd6d8857479
joe.mcilwraith <joe.mcilwraith@remarkable.net>
2024-07-13 13:20:47 +0100

    removed viewport from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import '../styles/globals.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import { Metadata, Viewport } from 'next'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Metadata } from 'next'
+import '../styles/globals.css'
 
 config.autoAddCss = false
 
@@ -13,14 +13,6 @@ export const metadata: Metadata = {
   icons: {
     icon: '/icon.png',
   },
-}
-
-export function generateViewPort() {
-  return {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-  }
 }
 
 export default function RootLayout(props: LayoutProps) {
